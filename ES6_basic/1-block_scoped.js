a5664d133db52642ed6f0d173a3820fdccc5ce66
Alison <8661@holbertonstudents.com>
2024-07-21 22:26:21 -0300

    arrow

--- ES6_basic/1-block_scoped.js
+++ ES6_basic/1-block_scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  const task2 = true;
-  
-  if (trueOrFalse) {
+let task = false;
+const task2 = true;
+    
+if (trueOrFalse) {
     let task = true;
     const task2 = false;
-  }
-  
-  return [task, task2];
+}
+    
+return [task, task2];
 }
