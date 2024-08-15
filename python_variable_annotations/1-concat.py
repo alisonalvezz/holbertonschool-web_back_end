dc00769516e147a0c3931faee27f6a6a32353b44
Alison <8661@holbertonstudents.com>
2024-08-15 08:53:55 -0300

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings
+    Attributes:
+        str1: first string
+        str2: second string
+    Returns:
+        The concatenated strings
+    """
+    return str1 + str2
