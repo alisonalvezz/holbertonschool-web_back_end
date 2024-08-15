c24af929b22dfa9b7173bdaeeddae2f39d3793f0
Alison <8661@holbertonstudents.com>
2024-08-15 08:59:31 -0300

    to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Function that takes a float and returns a str representation of it
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float and gives a str representation
+    Attributes:
+        n: the float to make the str representation
+    Returns:
+        A string representation of n
+    """
+    return str(n)
