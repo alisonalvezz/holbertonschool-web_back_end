30bdae9222520a24abe966b9318c8596afbd8bdb
Alison <8661@holbertonstudents.com>
2024-08-15 08:50:40 -0300

    add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function add
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Function that adds
+    Args:
+        a: a float
+        b: a float
+    Returns:
+        A float, the sum of a and b
+    """
+    return a + b
