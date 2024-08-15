37c97d833f00e0e624e9af3c25a024410243ffd4
Alison <8661@holbertonstudents.com>
2024-08-15 09:48:42 -0300

    to kv

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+type annotated function
+"""
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Function that takes a str 'k' and and int or float 'v'
+    Attributes:
+        k: str
+        v: float or int
+    Returns:
+        A tuple
+    """
+    return k, v * v
