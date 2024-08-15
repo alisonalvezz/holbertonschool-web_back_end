191f74316f415092b13bec197188b9caa76015aa
Alison <8661@holbertonstudents.com>
2024-08-15 08:56:55 -0300

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Function floor that returns the floor of a float
+    Attributes:
+        n: float
+    Returns:
+        The floor of n
+    """
+    return int(n)
