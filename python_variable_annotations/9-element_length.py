5994b87ecebfb48f78675e002d1d232875d34b9e
Alison <8661@holbertonstudents.com>
2024-08-16 14:53:09 -0300

    element length

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,7 +5,8 @@ insert variable annotations
 
 from typing import Sequence, Iterable, List, Tuple
 
-def element_length(lst: Iterable[Sequence]) ->List[Tuple[Sequence, int]]:
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Returns a list of tuples with their sequence and longitude
     """
