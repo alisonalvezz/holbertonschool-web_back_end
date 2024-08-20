56fc144dbd5386098a78a6bae136fdf65864b1a8
Alison <8661@holbertonstudents.com>
2024-08-20 16:50:04 -0300

    measure runtime

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+run time for four parallel comprehensions
+"""
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+import asyncio
+import time
+
+
+async def measure_runtime() -> float:
+    """
+    runs async_comprehensions four times in parallel
+    and returns the time that it takes
+    """
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.time()
+    return end_time - start_time
