5d2daee320eb6e7f2cb83d674177a50f66ce159a
Alison <8661@holbertonstudents.com>
2024-08-23 12:57:51 -0300

    i think it works now or so i hope

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,9 +2,10 @@
 """
 run time for four parallel comprehensions
 """
-async_comprehension = __import__('1-async_comprehension').async_comprehension
 import asyncio
 import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
