e1b8afcdc7c1ff27060255ee2e5fcb623d034614
alisonalvezz <alisonalvez05@gmail.com>
2024-09-06 15:14:41 -0300

    nosql

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,10 +4,10 @@ List all in MongoDB
 """
 
 def list_all(mongo_collection):
-  """
-  List all
-  """
-  doc = []
-  for _ in mongo_collection.find()
-    doc.append()
-  return doc
+    """
+    List all
+    """
+    doc = []
+    for _ in mongo_collection.find()
+        doc.append()
+    return doc
