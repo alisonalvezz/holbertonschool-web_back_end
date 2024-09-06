45976e46f9ae05e3515f206b447d2eb67c1cdef7
alisonalvezz <alisonalvez05@gmail.com>
2024-09-06 15:13:29 -0300

    nosql

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+List all in MongoDB
+"""
+
+def list_all(mongo_collection):
+  """
+  List all
+  """
+  doc = []
+  for _ in mongo_collection.find()
+    doc.append()
+  return doc
